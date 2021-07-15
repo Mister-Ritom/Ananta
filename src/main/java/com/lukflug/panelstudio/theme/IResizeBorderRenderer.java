288041c9cd8f25832b1f8cb545c1d4f4403d1c34
obama69420 <87466058+obama69420@users.noreply.github.com>
2021-07-15 11:22:40 +0430

    bigrat

--- src/main/java/com/lukflug/panelstudio/theme/IResizeBorderRenderer.java
+++
@@ -1,9 +0,0 @@
-package com.lukflug.panelstudio.theme;
-
-import com.lukflug.panelstudio.base.Context;
-
-public interface IResizeBorderRenderer {
-	public void drawBorder (Context context, boolean focus);
-	
-	public int getBorder();
-}
